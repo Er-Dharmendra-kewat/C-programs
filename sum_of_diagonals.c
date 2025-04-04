054cfa36e41a12e91adb4de083b17e1e68cb3370
Er-Dharmendra-kewat <techaboutneed0@gmail.com>
2025-04-04 17:15:12 +0530

    c programs

---
+++ sum_of_diagonals.c
@@ -0,0 +1,29 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // find the sum of diagonals elements
+    
+    int rows,cols;
+    int sum = 0;
+    printf("enter size of matrix:: ");
+    scanf("%d%d",&rows,&cols);
+   int a[rows][cols];
+    printf("enter %d by %d matrix:: ",rows,cols);
+     for(int i=0;i<rows;i++){
+             for(int j=0;j<cols;j++){
+               scanf("%d",&a[i][j]);  
+        }
+ }
+ for(int i=0;i<rows;i++){
+             for(int j=0;j<cols;j++){
+                 if(i==j){
+                     sum+=a[i][j];
+                 }
+                 
+        }
+ } 
+ printf("the sum of diagonals is::%d",sum);
+      
+    return 0;
+}
